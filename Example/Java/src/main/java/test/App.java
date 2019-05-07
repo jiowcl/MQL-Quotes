5592f586c0454e0294a703517357be6a415b095f
Jiowcl <jiowcl@gmail.com>
2019-05-07 15:58:32 +0800

    Add files via upload

---
+++ Example/Java/src/main/java/test/App.java
@@ -0,0 +1,49 @@
+package test;
+
+import org.zeromq.SocketType;
+import org.zeromq.ZMQ;
+import org.zeromq.ZContext;
+
+/**
+ * Test
+ */
+public final class App {
+    private App() {
+        
+    }
+
+    /**
+     * Main.
+     * @param args The arguments of the program.
+     */
+    public static void main(String[] args) {
+        ZContext context = new ZContext();
+        ZMQ.Socket subscriber = context.createSocket(SocketType.SUB);
+
+        Boolean enabled = true;
+        String serverAddr = "tcp://localhost:5559";
+
+        subscriber.connect(serverAddr);
+        subscriber.subscribe("");
+
+        while (enabled) {
+            String messageReceived = subscriber.recvStr(0).trim();
+            String[] messageData = messageReceived.split(" ");
+
+            if (messageData.length != 2)
+                continue;
+
+            String[] quoteData = messageData[1].toString().split("\\|");
+
+            if (quoteData.length != 4)
+                continue;
+
+            System.out.println("Login: " + messageData[0] + ", Symbol: " + quoteData[0] + ", Ask: " + quoteData[1] + ", Bid: " + quoteData[3] + ", Spread: " + quoteData[4]);
+        }
+
+        subscriber.disconnect(serverAddr);
+        subscriber.close();
+
+        context.close();
+    }
+}
